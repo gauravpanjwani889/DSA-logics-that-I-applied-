fb14953074054560386f28c428e030060a92dbd3
Gaurav Panjwani <gauravpanjwani889@gmail.com>
2026-07-04 02:22:23 +0530

    Time: 0 ms (100%), Space: 9.2 MB (12.92%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans=0;
+
+        for(int i=3;i<16;i+=3){
+            long long base=powl(10LL,i*1LL);
+            if(base>n){
+                break;
+            }
+            ans+=n-base+1;
+        }
+        return ans;
+        
+    }
+};
