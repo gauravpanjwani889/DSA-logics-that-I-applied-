0031c9e39de3a6c7f090845cc6759c530084bf32
Gaurav Panjwani <gauravpanjwani889@gmail.com>
2026-07-06 03:04:20 +0530

    Time: 0 ms (100%), Space: 31.3 MB (28.57%) - LeetHub

---
+++ 3978-unique-middle-element/3978-unique-middle-element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isMiddleElementUnique(vector<int>& nums) {
+        int cnt=0;
+        for(auto &x:nums){
+            cnt+=(x==nums[nums.size()/2]);
+        }
+        return cnt==1;
+        
+    }
+};
